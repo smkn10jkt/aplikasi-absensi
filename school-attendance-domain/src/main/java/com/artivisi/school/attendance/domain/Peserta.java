4793bd2cbe236ec4fd9357d742926baaa7e4fdc1
Endy Muhardin <endy.muhardin@gmail.com>
2013-01-30 11:38:42 +0700

    skema database

---
+++ school-attendance-domain/src/main/java/com/artivisi/school/attendance/domain/Peserta.java
@@ -0,0 +1,32 @@
+package com.artivisi.school.attendance.domain;
+
+import org.hibernate.annotations.GenericGenerator;
+import org.hibernate.validator.constraints.NotEmpty;
+
+import javax.persistence.*;
+import javax.validation.constraints.NotNull;
+
+@Entity
+@Table(name="t_peserta")
+public class Peserta {
+    @Id
+    @GeneratedValue(generator="system-uuid")
+    @GenericGenerator(name="system-uuid", strategy = "uuid2")
+    private String id;
+
+
+    @NotNull
+    @NotEmpty
+    @Column(nullable = false, unique = true)
+    private String nomor;
+
+    @NotNull
+    @NotEmpty
+    @Column(nullable = false)
+    private String nama;
+
+    @NotNull
+    @ManyToOne
+    @JoinColumn(name = "id_jenis_peserta", nullable = false)
+    private JenisPeserta jenisPeserta;
+}
